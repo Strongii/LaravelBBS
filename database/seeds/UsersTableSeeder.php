5770f5b0016fec1a2a74aa9d5d42a9fe7c569dcf
vagrant <vagrant@homestead>
2019-03-14 11:52:27 +0000

    UserSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -18,8 +18,12 @@ class UsersTableSeeder extends Seeder
         $avatars = [
             'http://www.yutudou.com/uploads/allimg/170809/1-1FP9221236.jpg',
             'http://8.pic.9ht.com/thumb/up/2018-4/201842795016108200_600_566.jpg',
-            'http://img.bqatj.com/img/e9372ad4a2bb0842.jpg',
+            'http://img3.imgtn.bdimg.com/it/u=1486068326,2690743681&fm=26&gp=0.jpg',
             'http://img.duoziwang.com/2016/10/02/1451111685.jpg',
+            'http://img.duoziwang.com/2017/10/1722593612686.jpg',
+            'http://www.touxiangdaquan.net/uploads/allimg/c150625/143521454V9320-4b249.jpg',
+            'http://img5.imgtn.bdimg.com/it/u=2587803502,3203790569&fm=26&gp=0.jpg',
+            'http://b-ssl.duitang.com/uploads/item/201708/29/20170829124617_XyhLs.thumb.1000_0.jpeg'
         ];
 
         // 生成数据集合
